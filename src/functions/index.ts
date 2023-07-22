3766d2afab3338e14cd2525bed172bbcae815fd0
arvidbt <arvid.bergman@sap.com>
2023-07-22 21:19:26 +0200

    remove log

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -4,10 +4,6 @@ import { getWordsStartingWith } from "./get_words_starting_with";
 import { getWordsEndingWith } from "./get_words_ending_with";
 import { getWordsOfLengthN } from "./get_words_of_length_n";
 
-
-console.log(getWordsContaining("hello", "en"))
-
-
 export {
   isWord,
   getWordsContaining,
