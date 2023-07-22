9dc5c7e276a592ca0603fa5e31bdd353ea235809
arvidbt <arvid.bergman@sap.com>
2023-07-22 13:05:31 +0200

    redefine exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { isWord } from "./functions/is_word";
-import { getWordsContaining } from "./functions/get_words_containing";
-import { getWordsStartingWith } from "./functions/get_words_starting_with";
-import { getWordsEndingWith } from "./functions/get_words_ending_with";
-import { getWordsOfLengthN } from "./functions/get_words_of_length_n";
+import {
+  isWord,
+  getWordsContaining,
+  getWordsStartingWith,
+  getWordsEndingWith,
+  getWordsOfLengthN,
+} from "./functions";
 
 export {
   isWord,
